e11da7d9bdfec7a3ba8e4668df5fc0a8fca04236
Abdullahi Muftau <abdullahimuftau2003@gmail.com>
2024-08-06 09:06:16 +0100

    hooks

--- src/routing/pages/recipes/index.jsx
+++ src/routing/pages/recipes/index.jsx
@@ -1,14 +1,29 @@
 import { useLocation } from "react-router-dom";
-
+import useFetch from "../../../hooks/UseFetch";
 
 const RecipeList = () => {
-    const location = useLocation()
-    console.log(location)
+  const location = useLocation();
+
+  const { data, loading, error } = useFetch("https://dummyjson.com/recipes");
+
+  // console.log(location);
+
+  if (loading) return <h1>Fetching Recipes Please Wait....</h1>;
   return (
     <div>
       <h1> Recipe List Page</h1>
+      <ul>
+        {data?.recipes?.length > 0
+          ? data?.recipes.map((recipeItem) => (
+              <div key={recipeItem.id}>
+                <img src={recipeItem.image} />
+                <label htmlFor="">{recipeItem.name}</label>
+              </div>
+            ))
+          : null}
+      </ul>
     </div>
   );
-}
+};
 
-export default RecipeList
+export default RecipeList;
